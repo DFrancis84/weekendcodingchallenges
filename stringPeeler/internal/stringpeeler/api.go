3cda25aa486fb9b886384ad8d7cfccd1e6126e6b
Devin Francis <devin.francis@eltoro.com>
2019-11-21 22:27:25 -0500

    updated to include timer

--- stringPeeler/internal/stringpeeler/api.go
+++ stringPeeler/internal/stringpeeler/api.go
@@ -3,6 +3,7 @@ package stringpeeler
 import (
 	"errors"
 	"fmt"
+	"time"
 )
 
 type StringPeelerAPI struct{}
@@ -12,10 +13,12 @@ func New() *StringPeelerAPI {
 }
 
 func (sp *StringPeelerAPI) StringPeeler(input string) (string, error) {
+	startTime := time.Now()
 	if len(input) <= 2 {
 		return "", errors.New(fmt.Sprintf("word must be larger than 2 characters long. current word '%v' is only %v characters long", input, len(input)))
 	}
 	output := input[:len(input)-1]
 	output = output[1:]
+	fmt.Printf("Peeler took %v to complete request for %v\n", time.Since(startTime), input)
 	return output, nil
 }
